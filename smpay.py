8ccf76fd393a537d4425e9527a7c82130ee64d39
xiaoyezi1992 <72925786+xiaoyezi1992@users.noreply.github.com>
2021-01-07 16:35:23 +0800

    Add files via upload

---
+++ smpay.py
@@ -0,0 +1,52 @@
+# _*_coding:utf-8_*_
+
+
+import pandas as pd
+import os
+
+
+path = 'E:/data/9-收入&成本核算/'
+period = input('请输入应收明细汇总月份：')
+open_path = path + period + '/实名支付'
+
+
+def total_doc(folder):
+    df = pd.DataFrame({})
+    for name in os.listdir(folder):
+        data = pd.read_excel(folder + '/' + name, header=2, usecols=['客户号', '客户名', '一级行业', '二级行业', '收入所属方',
+                                                                     '交易类型', '成功笔数(含跨行)', '成功金额(含跨行)',
+                                                                     '计费周期', '应收收入', '已收手续费', '未收手续费'])
+        df = pd.concat([df, data])
+    return df
+
+
+detail_org = total_doc(open_path)
+detail = detail_org[~(detail_org['客户号'].str.contains('合计：'))]
+detail = detail[~(detail['客户号'].str.contains('打印：'))]
+
+
+def cut(x):
+    if '卡中心' in x:
+        return x[0:x.find('卡中心') + 3]
+    elif '分行' in x:
+        return x[0:x.find('分行') + 2]
+    elif '公司' in x:
+        return x[0:x.find('公司') + 2]
+    elif '（' in x:
+        return x[0:x.find('（')]
+    else:
+        return x
+
+
+def str_flt(x):
+    if isinstance(x, str):
+        x = x.replace(',', '')
+    return float(x)
+
+
+detail['商户简称'] = detail['客户名'].astype(str).map(cut)
+detail.loc[:, '剔税已收'] = detail['已收手续费'].astype(str).map(str_flt)/1.06
+detail.loc[:, '剔税未收'] = detail['未收手续费'].astype(str).map(str_flt)/1.06
+save_detail = pd.ExcelWriter(open_path + '{}明细汇总.xlsx'.format(period))
+detail.to_excel(save_detail, '全部明细')
+save_detail.save()
